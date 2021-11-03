51e5c971047fa51a051c8b2767c6273a8931b850
felipeldn91 <felipeldn91@gmail.com>
2021-11-03 23:44:22 +0000

    state changes to user input on  onClick

--- lend-counter/src/components/Main.js
+++ lend-counter/src/components/Main.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
-import { Input } from 'semantic-ui-react'
-import { Button } from 'semantic-ui-react'
+import { Input, Button, Form } from 'semantic-ui-react'
 
 // Capture string entered by user - DONE
 // Call userString function when button is clicked
@@ -11,44 +10,61 @@ export default class Main extends Component {
         super(props)
 
         this.state = {
-            userString: ""
+            value: ''
         }
     
         this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     
     };
 
     handleChange(event) {
         this.setState({
-            userString: event.target.value
+            value: event.target.value
         })
     }
 
+    handleSubmit(event) {
+        console.log('The following was submitted:' + ` ${this.state.value}`)
+        event.preventDefault()
+    }
+
+
     render() {
 
-        const userSentence = this.state.userString
-        
-        const filterString = (string) => {
-            if (typeof string === 'string') {
-                console.log(string)
-            } else {
-                console.log('Please enter a string')
-            }
+        const filterString = (query) => {
+            console.log(query)
+            // if (typeof query === 'string') {
+            //     console.log(query)
+            // } else {
+            //     console.log('Please enter a string')
+            // }
         }
 
-        console.log(userSentence)
+        // const userSentence = this.state.userString
+
+        // console.log(userSentence)
 
         return(
             <div>
                 <p>Please enter a string in the text field below:</p>
 
-                <Input 
+                <Form onSubmit={this.handleSubmit}>
+                    <Form.Field onChange={this.handleChange}>
+                        <input placeholder="Enter a string"/>
+                    </Form.Field>
+                    <Button positive type='submit'>COUNT</Button>
+                </Form>
+
+                {/* <Input 
                     placeholder="Enter a string"
                     onChange={this.handleChange}
-                />
+                /> */}
+                    {/* <Button positive onClick={console.log("It's working!")}>COUNT</Button> */}
+                
                 <br/>
                 <br/>
-                <Button positive onClick={filterString(userSentence)}>COUNT</Button>
+                {/* <Button positive onClick={console.log("It's working!")}>COUNT</Button> */}
             </div>
         
         )
