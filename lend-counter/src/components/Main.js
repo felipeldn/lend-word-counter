99aac84eed97e3225b71449dca16acd1db61efa0
felipeldn91 <felipeldn91@gmail.com>
2021-11-03 22:31:57 +0000

    capturing state

--- lend-counter/src/components/Main.js
+++ lend-counter/src/components/Main.js
@@ -2,28 +2,55 @@ import React, {Component} from 'react'
 import { Input } from 'semantic-ui-react'
 import { Button } from 'semantic-ui-react'
 
-// Capture string entered by user 
-// Use handleChange 
+// Capture string entered by user - DONE
+// Call userString function when button is clicked
 
 export default class Main extends Component {
     
     constructor(props) {
         super(props)
 
-        this.state = ""
+        this.state = {
+            userString: ""
+        }
+    
+        this.handleChange = this.handleChange.bind(this)
+    
     };
 
+    handleChange(event) {
+        this.setState({
+            userString: event.target.value
+        })
+    }
+
     render() {
+
+        const userSentence = this.state.userString
+        
+        const filterString = (string) => {
+            if (typeof string === 'string') {
+                console.log(string)
+            } else {
+                console.log('Please enter a string')
+            }
+        }
+
+        console.log(userSentence)
 
         return(
             <div>
                 <p>Please enter a string in the text field below:</p>
 
-                <Input placeholder="Enter a string"/>
+                <Input 
+                    placeholder="Enter a string"
+                    onChange={this.handleChange}
+                />
                 <br/>
                 <br/>
-                <Button positive>COUNT</Button>
+                <Button positive onClick={filterString(userSentence)}>COUNT</Button>
             </div>
+        
         )
 
     }
