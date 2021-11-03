ac0de4aaa0c50ba8c77c552ab2fc6989184fd616
felipeldn91 <felipeldn91@gmail.com>
2021-11-03 22:00:06 +0000

    component chabged to class component

--- lend-counter/src/components/Main.js
+++ lend-counter/src/components/Main.js
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, {Component} from 'react'
 import { Input } from 'semantic-ui-react'
 import { Button } from 'semantic-ui-react'
 
-function Main() {
-    return (
-        <div>
-            <p>Please enter a string in the text field below:</p>
+// Capture string entered by user 
+// Use handleChange 
 
-            <Input placeholder="Enter a string"/>
-        <br/>
-        <br/>
-            <Button positive>COUNT</Button>
-        </div>
-    )
+export default class Main extends Component {
+    
+    constructor(props) {
+        super(props)
+
+        this.state = ""
+    };
+
+    render() {
+
+        return(
+            <div>
+                <p>Please enter a string in the text field below:</p>
+
+                <Input placeholder="Enter a string"/>
+                <br/>
+                <br/>
+                <Button positive>COUNT</Button>
+            </div>
+        )
+
+    }
+    
 }
 
-export default Main
+
