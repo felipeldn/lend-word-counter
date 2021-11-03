9edddf6bf959a586c9207555a7bfb5606c45e184
felipeldn91 <felipeldn91@gmail.com>
2021-11-03 21:37:09 +0000

    semantic ui

--- lend-counter/src/App.js
+++ lend-counter/src/App.js
@@ -1,22 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+import { Input } from 'semantic-ui-react'
+
+// Display title - DONE 
+// Display caption - DONE
+// Display text field - DONE
+// Change state to entered text - DONE 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>REACT WORD COUNTER</h1>
+        <p>Please enter a string in the text field below:</p>
+
       </header>
     </div>
   );
